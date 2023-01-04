4f2ad50d19616b56c2c0508b24377b5489062a0e
Igor Ryzhenkov <27rogi@pm.me>
2023-01-04 16:18:26 +0300

    code cleanup

--- src/main/java/space/rogi27/renameit/RenameIt.java
+++ src/main/java/space/rogi27/renameit/RenameIt.java
@@ -6,9 +6,6 @@ import org.apache.logging.log4j.Logger;
 import space.rogi27.renameit.commands.RenameItCommand;
 
 public class RenameIt implements ModInitializer {
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LogManager.getLogger("RenameIt");
 
     @Override
